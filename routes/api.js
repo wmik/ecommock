8317056de2ff3e643a42f4d1dd74a8880521382f
wmik <williemik.wmik@gmail.com>
2019-03-25 12:36:58 +0300

    fix: incorrect status response for missing data
    - Load data dynamically

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,12 @@
 var env = process.env.NODE_ENV || 'test';
+var path = require('path');
 var paige = require('../utils/pagination');
 var shallow = require('../utils/shallow');
-var data = require('../data.json');
 var config = require('../config.' + env + '.json');
+var data = require(path.resolve(config.data.filePath));
 
 function get(q, s, x) {
-  var resource, args, items, response;
+  var resource, args, items, response, opts;
   resource = q.params.resource;
   args = {
     id: q.params[0] || q.query.id || null
@@ -16,19 +17,20 @@ function get(q, s, x) {
     return s.status(404).json(response);
   }
   if (items && !args.id) {
-    response = paige.multi(
-      shallow(config.api.pagination, {
-        page: q.query.page,
-        per_page: q.query.per_page
-      }),
-      data[resource]
-    );
+    opts = shallow(config.api.pagination, {
+      page: q.query.page,
+      per_page: q.query.per_page
+    });
+    response = paige.multi(opts, data[resource]);
   } else if (items && args.id) {
     response = paige.single(data[resource]);
   }
-  return s.json(response);
+  return s.status(response.data ? 200 : 404).json(response);
 }
 
+function post(q, s, x) {}
+
 module.exports = {
-  get
+  get,
+  post
 };
